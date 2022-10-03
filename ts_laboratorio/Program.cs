daf7b380a3b28d71557fc87dca4a1aa830235064
luise <luise@DESKTOP-678KKUR>
2022-10-02 22:09:16 -0600

    Agregar archivos de proyecto.

---
+++ ts_laboratorio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ts_laboratorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant;
+            int a, b, c;
+            Console.WriteLine("Ingresar numero: ");
+            cant = Int32.Parse(Console.ReadLine());
+            a = 0; b = 1;
+            Console.Write(a + " " + b + " " );
+            for (int k = 3; k<= cant; k++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+        }
+    }
+}
